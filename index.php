1d4acf4261e67ee4cb60b1246171b69977c2a8a4
nema <40147733+nexnema@users.noreply.github.com>
2021-12-13 18:29:53 +0330

    rty

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
 <?php
-$x = 24.4;
 $y = " Hello World!";
 
-$z = $x.$y;
+$z = strlen($y);
 ?>
 
 <h1><?=$z?></h1>
 
-<?php
-$a = print "asyfgsi gag f";
-?>
-
-<hr>
-
-<?php
-var_dump($yhdrtdfuygk);
