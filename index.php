1441d2809884eb934653872893e5170e33033f07
nema <40147733+nexnema@users.noreply.github.com>
2021-12-13 18:52:23 +0330

    d

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
-$x = 0;
-
-
-while ($x <= 100) {
-    echo $x;
-    echo "<hr>";
-
-    $x = $x + 1;
+function namyeshbede($x) {
+    while ($x <= 100) {
+        echo $x;
+        echo "<hr>";
+    
+        $x = $x + 1;
+    }
 }
+?>
+
+
+
+
+<?php
+namyeshbede(44);
 ?>
 
 
