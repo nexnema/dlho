8a8d0f699aea5ddc6ce762f6a248d53f66385050
nema <40147733+nexnema@users.noreply.github.com>
2022-01-03 16:48:21 +0330

    b

--- index.php
+++ index.php
@@ -1,26 +1,3 @@
 <?php
-function factoryel($x) {
-    $i = 1;
-    $z = 1;
-    while ($i <= $x) {
-        $z = $z * ++$i;
-    }
-    // return $z;
-}
-?>
 
-
-
-
-<?php
-echo factoryel(4);
-?>
-<hr>
-<?php
-$g = factoryel(10);
-
-echo $g;
-
-?>
-
-
+echo "Hello World!";
